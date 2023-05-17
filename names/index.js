913ea1a6dce1fa40e3b0d335e85214c9a76ef59d
ssaiprasad01 <ssaiprasad01@gmail.com>
2023-05-17 10:56:26 +0000

    first Names of the people

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const peopleNames = require("../country/state/city/index");
+const firstNames = require("../utilities/utils/index");
+const getPeopleInCity = (peopleNames) => {
+  return firstNames(peopleNames);
+};
+module.exports = getPeopleInCity;
